47d8bf62937e4ccdee03a432ed084c5938a2896c
zer0who <eagle7449@naver.com>
2022-12-16 20:23:36 +0900

    first commit

---
+++ src/main/java/com/example/spring_prac1/SpringPrac1Application.java
@@ -0,0 +1,14 @@
+package com.example.spring_prac1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPrac1Application {
+
+	public static void main(String[] args) {
+		System.out.print("Hello World");
+		SpringApplication.run(SpringPrac1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_prac1/SpringPrac1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_prac1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPrac1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
